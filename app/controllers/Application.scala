1c5cebe5167177a9c1e89976dfbc8b6887bfc4fa
Marius Soutier <spybreak@gmx.net>
2014-08-09 13:17:02 +0200

    Get controller routes via reflection http://stackoverflow.com/questions/12012703/less-verbose-way-of-generating-play-2s-javascript-router

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -18,24 +18,27 @@ object Application extends Controller with Security {
   }
 
   /**
+   * Retrieves all routes via reflection.
+   * http://stackoverflow.com/questions/12012703/less-verbose-way-of-generating-play-2s-javascript-router
+   */
+  val routeCache = {
+    val jsRoutesClass = classOf[routes.javascript]
+    val controllers = jsRoutesClass.getFields.map(_.get(null))
+    controllers.flatMap { controller =>
+      controller.getClass.getDeclaredMethods.map { action =>
+        action.invoke(controller).asInstanceOf[play.core.Router.JavascriptReverseRoute]
+      }
+    }
+  }
+
+  /**
    * Returns the JavaScript router that the client can use for "type-safe" routes.
    * Uses browser caching; set duration (in seconds) according to your release cycle.
    * @param varName The name of the global variable, defaults to `jsRoutes`
    */
   def jsRoutes(varName: String = "jsRoutes") = Cached(_ => "jsRoutes", duration = 86400) {
     Action { implicit request =>
-      Ok(
-        Routes.javascriptRouter(varName)(
-          routes.javascript.Application.login,
-          routes.javascript.Application.logout,
-          routes.javascript.Users.authUser,
-          routes.javascript.Users.user,
-          routes.javascript.Users.createUser,
-          routes.javascript.Users.updateUser,
-          routes.javascript.Users.deleteUser
-          // TODO Add your routes here
-        )
-      ).as(JAVASCRIPT)
+      Ok(Routes.javascriptRouter(varName)(routeCache: _*)).as(JAVASCRIPT)
     }
   }
 
