00b63c8c9d78a6df0f9712baf8d262ce72428a37
Marius Soutier <spybreak@gmx.net>
2015-06-29 13:51:39 +0200

    Always use JodaDate

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,15 +1,14 @@
 package models
 
-//import org.joda.time.DateTime
-import java.util.Date
+import org.joda.time.DateTime
 
 case class User(
   id: Option[Long],
   email: String,
   password: String,
   name: String,
-  dateOfBirth: Option[Date],
-  createdAt: Date = new Date
+  dateOfBirth: Option[DateTime],
+  createdAt: DateTime = DateTime.now()
 )
 
 object User {
@@ -22,21 +21,21 @@ object User {
     (__ \ "email").read(Reads.email) ~
     (__ \ "password").read[String] ~
     (__ \ "name").read[String] ~
-    (__ \ "dateOfBirth").readNullable[Date] ~
-    (__ \ "createdAt").read(new Date)
+    (__ \ "dateOfBirth").readNullable[DateTime] ~
+    (__ \ "createdAt").read(DateTime.now())
   )(User.apply _)
 
   implicit val UserToJson: Writes[User] = (
     (__ \ "id").writeNullable[Long] ~
     (__ \ "email").write[String] ~
-    (__ \ "password").writeNullable[String] ~ // don't write the password
+    (__ \ "password").writeNullable[String] ~ // make nullable so password can be omitted
     (__ \ "name").write[String] ~
-    (__ \ "dateOfBirth").writeNullable[Date] ~
-    (__ \ "createdAt").write[Date]
+    (__ \ "dateOfBirth").writeNullable[DateTime] ~
+    (__ \ "createdAt").write[DateTime]
   )((user: User) => (
     user.id,
     user.email,
-    None,
+    None, // here we skip the password
     user.name,
     user.dateOfBirth,
     user.createdAt
